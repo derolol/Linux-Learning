06d6df01804b91f47694cfdb0e5f1db7eb0b5faf
derolol <837456540@qq.com>
2020-11-08 23:29:10 -0800

    add week8 code and note

--- week6/code/6-7.c
+++ week6/code/6-7.c
@@ -2,23 +2,39 @@
 
 // [PROBLEM] How to let the program print 'test' when second search
 // setenv(name, value, rewrite)
-// putenv
+// putenv Linux下putenv只对当前函数有效
+// setenv setenv函数设置的环境变量只在本进程，而且是本次执行中有效
 // unsetenv
 
+int i = 1;
+
+// 通过setenv对本次进程的环境变量进行设置
+static void set_env_string_edit(void) {
+	char name[] = "test_env";
+	char value[] = "test";
+	if (setenv(name, value, 1) != 0) {
+		printf("Setenv is failed!\n");
+	}
+	printf("%d.[setenv]The test_env = %s\n", i++, getenv("test_env"));
+}
+
+// 通过putenv对本函数执行时的环境变量进行设置
 static void set_env_string(void) {
 	char test_env[] = "test_env=test";
 	if (putenv(test_env)!=0) {
 		printf("Putenv is failed!\n");
 	}
-	printf("1. The test_env = %s\n", getenv("test_env"));
+	printf("%d.[putenv]The test_env = %s\n", i++, getenv("test_env"));
 }
 
 static void show_env_string() {
-	printf("2. The test_env = %s\n", getenv("test_env"));
+	printf("%d.The test_env = %s\n", i++, getenv("test_env"));
 }
 
 int main() {
 	set_env_string();
+	show_env_string();
+	set_env_string_edit();
 	show_env_string();
 	return 0;
 }
--- week6/code/error/error2.c
+++ week6/code/error/error2.c
@@ -1,12 +1,32 @@
 #include "../ch6.h"
 #define N 10
+
+// 编译的时候全局变量的内存地址和宽度就已经确定了
+// 存储在内存的全局数据区，而且地址是连续分配的
+// []是下标运算符，将首地址加上偏移量的地址计算出来
+// a[N]的地址和i的地址是相同的，所以对a[N]和对i操作都是相同的
+
 int a[N], i;
 int main() {
 	for (i = 0; i < N; i ++) {
 		a[i] = i+1;
 	}
+
+	for (i = 0; i < N; i++) {
+		printf("&a[%d] = %p\n", i, &a[i]);
+	}
+	printf("&a[%d] = %p\n", N, &a[N]);
+	printf("&i = %p\n", &i);
+
+	printf("\n执行 a[N] = 123456789\n");
 	a[N] = 123456789;
 	printf("i = %d\n", i);
-	printf("%d\n", a[i]);
+	printf("a[N] = %d\n", a[N]);
+
+	printf("\n执行 i = 9999\n");
+	i = 9999;
+	printf("i = %d\n", i);
+	printf("a[N] = %d\n", a[N]);
+
 	return 0;
 }
